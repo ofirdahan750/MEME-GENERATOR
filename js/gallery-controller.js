6b628027436707280eaeaadeed3b976061464eb2
Your Name <you@example.com>
2021-02-11 12:04:30 +0200

    render grid+grid style-still need to  add  media quray

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,5 +1,24 @@
 'use strict'
 
-window.onload = renderImg() {
+window.onload = function() {
+  console.log('hi')
+    createImage();
+    renderImg();
+  };
 
+function renderImg() {
+var imgs = getImg()
+var strHtml=imgs.map(function(img) {
+  return `
+  <div class="grid-warp">
+  <div class="picture" style="background-image: url(${img.url})"></div>
+  </div>
+  `
+  
+})
+document.querySelector('.grid-container').innerHTML=strHtml.join('')
 }
+
+// return `<div class="picture picture${img.id}">
+// <img src="${img.url}" >
+// </div>`
--- js/gallery-service.js
+++ js/gallery-service.js
@@ -1,10 +1,42 @@
 'use strict'
 const IMG_KEY = 'imgs';
-window.onload=_createImage()
-function _createImage() { 
-var gImg = getFromStorage(IMG_KEY)
-if (!img || !img.length) {
-    img=[]
+var gImg
+
+
+
+function createImage() {
+    var imgs = getFromStorage(IMG_KEY);
+    if (!imgs || !imgs.length) {
+        var imgs = [
+            { id: 1, url: 'meme-imgs/1.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/2.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/3.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/4.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/5.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/6.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/7.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/8.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/9.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/10.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/11.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/12.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/13.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/14.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/15.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/16.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/17.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/18.jpg', keywords: ['happy,dance']},
+            { id: 1, url: 'meme-imgs/19.jpg', keywords: ['happy,dance']}
+        ]
+    }
+    gImg = imgs;
+    _saveImgToStorage();
 }
-var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
+
+function _saveImgToStorage() {
+    saveToStorage(IMG_KEY, gImg)
 }
+
+function getImg() {
+    return gImg
+} 
